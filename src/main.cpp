fe238d3aab0d850a5ce3589f8d9c6559f18e715e
vorgestern <fisches@gmx.de>
2026-09-04 01:45:03 +0200

    Updated dependencies, adapted

--- src/main.cpp
+++ src/main.cpp
@@ -23,30 +23,6 @@ int demofail(lua_State*L)
 #define ALLTAG_EXPORTS
 #endif
 
-static int mapper(lua_State*L)
-{
-    LuaStack Q(L);
-    if (height(Q)==0) return 0;
-    if (Q.typeat(-2)!=LuaType::TTABLE) return 0;
-    Q<<lualist<<lualistend<<luarot_3; // ==> [func {} list]
-    auto func=Q.index(-3), mappedlist=Q.index(-2);
-    for (LuaIterator J(Q); next(J); ++J)
-    {
-        // Call func(item)
-        // Catch nil-results, because they are not permissible in mapping.
-        Q<<LuaFuncValue(stackindex(func))<<LuaValue(-2)>>1;
-        if (Q.hasnilat(-1))
-        {
-            auto item=Q.index(-2);
-            Q<<LuaGlobal("string")<<LuaDotCall("format")<<"map: function returns nil for element %d, which is: %s"<<(unsigned)J<<item>>1;
-            Q>>luaerror;
-        }
-        Q>>LuaElement({stackindex(mappedlist), (unsigned)J}); // add result to mappedlist.
-    }
-    Q<<mappedlist;
-    return 1;
-}
-
 const auto keymap_impl=LuaCode(R"__(
 return function(L, func)
     if not L then return end
@@ -141,7 +117,7 @@ extern "C" ALLTAG_EXPORTS int luaopen_alltag(lua_State*L)
         <<formatany>>LuaField("formatany")
         <<keyescape>>LuaField("keyescape")
         <<demofail>>LuaField("demofail") // Produziert eine Fehlermeldung aus einem Aufruf von LuaAide.
-        <<mapper>>LuaField("map");
+        <<map>>LuaField("map");
 
     Q<<make_pair("keymap-impl", keymap_impl)>>1; Q>>LuaField("keymap");
     Q<<make_pair("apply-impl", apply_impl)>>1; Q>>LuaField("apply");
