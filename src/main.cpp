042ad7cf08e44345d29de4e8ed7959a71e7b613f
vorgestern <fisches@gmx.de>
2026-09-12 03:10:52 +0200

    Updated dependencies, adapted

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int demofail(lua_State*L)
 {
     LuaStack Q(L);
     // This Script cannot be compiled (missing parenthesis).
-    Q<<make_pair("Failing Demo", LuaCode(R"xxx(
+    Q<<LuaCode("Failing Demo", R"xxx(
         function translate(A,
             return 1
         end
-    )xxx"))>>0;
+    )xxx")>>0;
     Q<<1;
     return 1;
 }
@@ -20,23 +20,23 @@ int demofail(lua_State*L)
 #define ALLTAG_EXPORTS
 #endif
 
-const auto keymap_impl=LuaCode(R"__(
+const auto keymap_impl=R"__(
 return function(L, func)
     if not L then return end
     local A={}
     for k,v in pairs(L) do A[k]=func(v, k) end
     return A
 end
-)__");
+)__";
 
-const auto applypairs_impl=LuaCode(R"__(
+const auto applypairs_impl=R"__(
 return function(L, proc)
     if not L then return end
     for k,v in pairs(L) do proc(k,v) end
 end
-)__");
+)__";
 
-const auto findfirst_impl=LuaCode(R"__(
+const auto findfirst_impl=R"__(
 return function(L, pred)
     if not L then return end
     pred=pred or function(x) return x end
@@ -44,16 +44,16 @@ return function(L, pred)
         if pred(v) then return v,k end
     end
 end
-)__");
+)__";
 
-const auto contains_impl=LuaCode(R"__(
+const auto contains_impl=R"__(
 return function(L, item)
     if not L or not item then return end
     for k,v in pairs(L) do if v==item then return k end end
 end
-)__");
+)__";
 
-const auto filter_impl=LuaCode(R"__(
+const auto filter_impl=R"__(
 return function(L, pred)
     if not L then return end
     if not pred then return {} end
@@ -61,9 +61,9 @@ return function(L, pred)
     for k,v in ipairs(L) do if pred(v) then table.insert(result,v) end end
     return result
 end
-)__");
+)__";
 
-const auto pipe_lines_impl=LuaCode(R"__(
+const auto pipe_lines_impl=R"__(
 return function(command, linehandler)
     if type(command)~="string" then
         error("Error: pipe_lines: First argument must be a string (command) but is of type "..type(command)..".")
@@ -79,9 +79,9 @@ return function(command, linehandler)
         error(string.format("Error running '%s'", command))
     end
 end
-)__");
+)__";
 
-const auto sortedpairs_impl=LuaCode(R"__(
+const auto sortedpairs_impl=R"__(
 return function(X, sorter)
     local Copy={}
     if X then
@@ -98,7 +98,7 @@ return function(X, sorter)
         return state.K[state.control][1], state.K[state.control][2]
     end, {K=Copy}
 end
-)__");
+)__";
 
 extern "C" ALLTAG_EXPORTS int luaopen_alltag(lua_State*L)
 {
@@ -114,12 +114,12 @@ extern "C" ALLTAG_EXPORTS int luaopen_alltag(lua_State*L)
         <<map>>LuaField("map")
         <<apply>>LuaField("apply");
 
-    Q<<make_pair("keymap-impl", keymap_impl)>>1; Q>>LuaField("keymap");
-    Q<<make_pair("applypairs-impl", applypairs_impl)>>1; Q>>LuaField("applypairs");
-    Q<<make_pair("findfirst-impl", findfirst_impl)>>1; Q>>LuaField("findfirst");
-    Q<<make_pair("contains-impl", contains_impl)>>1; Q>>LuaField("contains");
-    Q<<make_pair("filter-impl", filter_impl)>>1; Q>>LuaField("filter");
-    Q<<make_pair("pipe_lines-impl", pipe_lines_impl)>>1; Q>>LuaField("pipe_lines");
-    Q<<make_pair("sortedpairs-impl", sortedpairs_impl)>>1; Q>>LuaField("sortedpairs");
+    Q<<LuaCode("keymap-impl", keymap_impl)>>1; Q>>LuaField("keymap");
+    Q<<LuaCode("applypairs-impl", applypairs_impl)>>1; Q>>LuaField("applypairs");
+    Q<<LuaCode("findfirst-impl", findfirst_impl)>>1; Q>>LuaField("findfirst");
+    Q<<LuaCode("contains-impl", contains_impl)>>1; Q>>LuaField("contains");
+    Q<<LuaCode("filter-impl", filter_impl)>>1; Q>>LuaField("filter");
+    Q<<LuaCode("pipe_lines-impl", pipe_lines_impl)>>1; Q>>LuaField("pipe_lines");
+    Q<<LuaCode("sortedpairs-impl", sortedpairs_impl)>>1; Q>>LuaField("sortedpairs");
     return 1;
 }
